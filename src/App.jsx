30731f852128fa80cbe50ad224d7409fdde19cb7
ashik-08 <ashikurrahman.8october@gmail.com>
2024-11-04 23:31:33 +0600

    [feat]: add assets, icons and styles, update font and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1>Vite + React</h1>;
+  return <h1 className="text-3xl">Vite + React</h1>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      container: {
+        center: true,
+        padding: "1.25rem",
+      },
+      colors: {
+        primary: "#B4D429",
+        dark: "#171923",
+        light: "#fff",
+        body: "#1D1E28",
+      },
+    },
   },
   plugins: [],
 };
