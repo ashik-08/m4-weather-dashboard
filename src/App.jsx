682f9652dd236e7810c9518a8507de542f72ed52
ashik-08 <ashikurrahman.8october@gmail.com>
2024-11-05 02:58:41 +0600

    [feat](context): add WeatherProvider and WeatherContext for weather data management

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import Header from "./components/Header/Header";
 import WeatherBoard from "./components/Weather/WeatherBoard";
+import WeatherProvider from "./provider/WeatherProvider";
 
 function App() {
   return (
-    <div className="h-screen grid place-items-center">
-      <Header />
-      <main>
-        <section>
-          <WeatherBoard />
-        </section>
-      </main>
-    </div>
+    <WeatherProvider>
+      <div className="h-screen grid place-items-center">
+        <Header />
+        <main>
+          <section>
+            <WeatherBoard />
+          </section>
+        </main>
+      </div>
+    </WeatherProvider>
   );
 }
 
--- src/components/Weather/WeatherBoard.jsx
+++ src/components/Weather/WeatherBoard.jsx
@@ -1,19 +1,26 @@
-import useWeather from "../../hooks/useWeather";
+import { useContext } from "react";
+import { WeatherContext } from "../../context";
 import AddToFavorite from "./AddToFavorite";
 import WeatherCondition from "./WeatherCondition";
 import WeatherHeadline from "./WeatherHeadline";
 
 const WeatherBoard = () => {
-  const { loading, error, weatherData } = useWeather();
+  const { loading, error, weatherData } = useContext(WeatherContext);
   console.log({ loading, error, weatherData });
 
   return (
     <div className="container">
       <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto">
         <div className="grid md:grid-cols-2 gap-10 md:gap-6">
-          <AddToFavorite />
-          <WeatherHeadline />
-          <WeatherCondition />
+          {loading.state ? (
+            <p>{loading.message}</p>
+          ) : (
+            <>
+              <AddToFavorite />
+              <WeatherHeadline />
+              <WeatherCondition />
+            </>
+          )}
         </div>
       </div>
     </div>
