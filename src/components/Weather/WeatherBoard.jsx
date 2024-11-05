781af4dc0ecd55b1cc166c02d08bca5edad5fc70
ashik-08 <ashikurrahman.8october@gmail.com>
2024-11-05 06:52:51 +0600

    [feat](header): implement favorite locations modal and update favorite component

--- src/components/Weather/WeatherBoard.jsx
+++ src/components/Weather/WeatherBoard.jsx
@@ -9,8 +9,8 @@ const WeatherBoard = () => {
 
   return (
     <div className="container">
-      <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto">
-        <div className="grid md:grid-cols-2 gap-10 md:gap-6">
+      <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-5 md:px-8 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1140px] mx-auto">
+        <div className="grid md:grid-cols-2 gap-10 md:gap-8 lg:gap-x-16">
           {loading.state ? (
             <p>{loading.message}</p>
           ) : (
--- src/components/Weather/WeatherHeadline.jsx
+++ src/components/Weather/WeatherHeadline.jsx
@@ -2,11 +2,11 @@ import { useContext } from "react";
 import CloudIcon from "../../assets/cloud.svg";
 import DrizzleIcon from "../../assets/drizzle.svg";
 import HazeIcon from "../../assets/haze.svg";
+import PinIcon from "../../assets/pin.svg";
 import RainIcon from "../../assets/rainy.svg";
 import SnowIcon from "../../assets/snow.svg";
 import SunIcon from "../../assets/sun.svg";
 import ThunderIcon from "../../assets/thunder.svg";
-import PinIcon from "../../assets/pin.svg";
 import { WeatherContext } from "../../context";
 import { getFormattedDate } from "../../utils/date-util";
 
@@ -41,15 +41,21 @@ const WeatherHeadline = () => {
 
   return (
     <div>
-      <div className="max-md:flex items-center justify-between md:-mt-10">
-        <img src={getWeatherIcon(climate)} alt="climate" />
-        <div className="max-md:flex items-center max-md:space-x-4">
-          <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
+      <div className="md:-mt-10">
+        <img
+          className="w-10 xl:w-14"
+          src={getWeatherIcon(climate)}
+          alt="climate"
+        />
+        <div className="max-md:flex items-center max-md:space-x-5">
+          <h1 className="text-[52px] md:text-[68px] lg:text-[80px] xl:text-[90px] leading-none md:mb-4">
             {Math.round(temperature)}°
           </h1>
-          <div className="flex items-center space-x-4 md:mb-4">
-            <img src={PinIcon} alt="pin" />
-            <h2 className="text-2xl lg:text-[50px]">{location}</h2>
+          <div className="flex items-center space-x-2 md:mb-5">
+            <img className="w-6 xl:w-8" src={PinIcon} alt="pin" />
+            <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+              {location}
+            </h2>
           </div>
         </div>
       </div>
