e5e530768df598fcfec4cd23e3eaeee30c44688f
ashik-08 <ashikurrahman.8october@gmail.com>
2024-11-05 09:25:42 +0600

    [feat](search): implement debounced search functionality and refactor search handling

--- src/components/Header/Search.jsx
+++ src/components/Header/Search.jsx
@@ -1,23 +1,34 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import SearchIcon from "../../assets/search.svg";
 import { LocationContext } from "../../context";
 import { getLocationByName } from "../../data/location-data";
+import useDebounce from "../../hooks/useDebounce";
 
 const Search = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  // const [searchTerm, setSearchTerm] = useState("");
   const { setSelectedLocation } = useContext(LocationContext);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const FetchLocation = getLocationByName(searchTerm);
-    setSelectedLocation(FetchLocation);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const FetchLocation = getLocationByName(searchTerm);
+  //   setSelectedLocation(FetchLocation);
+  // };
+
+  const doSearch = useDebounce((term) => {
+    const fetchedLocation = getLocationByName(term);
+    setSelectedLocation({ ...fetchedLocation });
+  }, 2000);
+
+  function handleChange(e) {
+    const value = e.target.value;
+    doSearch(value);
+  }
 
   return (
-    <form action="#" onSubmit={handleSubmit}>
+    <form action="#">
       <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
         <input
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={handleChange}
           className="bg-transparent placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
           type="search"
           placeholder="Search Location"
